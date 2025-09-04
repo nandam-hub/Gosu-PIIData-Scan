e8a0725ac14076c1aac228cf0bb8b602d46bf1de
nandam-hub <nandakishore.marri@valuemomentum.com>
2025-09-04 09:40:33 +0000

    Update build-app.bat

--- build-app.bat
+++ build-app.bat
@@ -1,12 +1,15 @@
 @echo off
 echo Building Gosu applications...
 echo.
+
 echo Creating distribution directory...
 mkdir dist 2>nul
-copy AddNumbers.gs dist\
-copy AddNumbersUI.gs dist\
-copy gosu.bat dist\
-xcopy gosu dist\gosu\ /E /I /Q
+
+copy /Y AddNumbers.gs dist\
+copy /Y AddNumbersUI.gs dist\
+copy /Y gosu.bat dist\
+xcopy gosu dist\gosu\ /E /I /Q /Y
+
 echo.
 echo Build complete! Applications are in the 'dist' directory.
 echo Run: dist\gosu.bat AddNumbers.gs
